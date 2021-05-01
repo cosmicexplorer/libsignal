98d66d5742fcd8119d8b765ade5f5460581e18e4
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2021-05-01 06:42:01 -0700

    Add docs to `proto`!

--- rust/protocol/src/proto.rs
+++ rust/protocol/src/proto.rs
@@ -1,7 +1,9 @@
 //
-// Copyright 2020 Signal Messenger, LLC.
+// Copyright 2020-2021 Signal Messenger, LLC.
 // SPDX-License-Identifier: AGPL-3.0-only
 //
+
+//! Protobuf definitions.
 
 pub mod fingerprint;
 pub mod sealed_sender;
