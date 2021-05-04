52ef586c42f8dbbd4b9624ccb527d361822d8256
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2021-05-04 11:30:24 -0700

    introduce `DeviceId` wrapper struct

--- rust/protocol/src/address.rs
+++ rust/protocol/src/address.rs
@@ -12,11 +12,31 @@ use crate::SignalMessage;
 
 use std::fmt;
 
-/// The type used in memory to represent a *device*, i.e. a particular Signal client instance which
-/// represents some user.
+/// A *device* is a particular Signal client instance which represents some user.
 ///
-/// Used in [ProtocolAddress].
-pub type DeviceId = u32;
+/// For example, a user may have set up Signal on both their phone and laptop. Any
+/// [crate::SignalMessage] sent to the user will still only go to a single device, so when a user
+/// sends a message to another user at all, they're actually sending a message to *every* device.
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, PartialOrd, Ord)]
+pub struct DeviceId(u32);
+
+impl From<u32> for DeviceId {
+    fn from(value: u32) -> Self {
+        Self(value)
+    }
+}
+
+impl From<DeviceId> for u32 {
+    fn from(value: DeviceId) -> Self {
+        value.0
+    }
+}
+
+impl fmt::Display for DeviceId {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 /// Represents a unique Signal client instance as `(<user ID>, <device ID>)` pair.
 #[derive(Clone, Debug, Hash, Eq, PartialEq, PartialOrd, Ord)]
